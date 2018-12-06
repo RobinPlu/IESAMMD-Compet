6d262c63c67d75c04735ba9beeef9cb436ee76ff
Robin PLU <42034512+RobinPlu@users.noreply.github.com>
2018-12-06 16:01:39 +0100

    optimisation

--- js/script.js
+++ js/script.js
@@ -7,4 +7,6 @@ $('#checkbox').click(function(event) {
     $('.nl-mobile').removeClass('active');
 });
 
+var images = ['fond-1-min.jpg', 'fond-2-min.jpg', 'fond-3-min.jpg'];
+$('.coverPage').css({'background-image': 'url(image/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
